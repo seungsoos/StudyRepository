7b36fe54c82ac2fe8882ec71f620bc17d68459a0
seungsoo <seung_soos@naver.com>
2025-04-27 23:06:07 +0900

    feat : TestContainer 적용

--- src/test/java/com/example/container/TestContainerConfig.java
+++ src/test/java/com/example/container/TestContainerConfig.java
@@ -1,15 +1,38 @@
 package com.example.container;
 
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
-//@Testcontainers
+@TestConfiguration
 public class TestContainerConfig {
 
-//    @Container
-//    @ServiceConnection(name = "redis")
-    public static final GenericContainer<?> REDIS_CONTAINER = new GenericContainer<>("redis:7.2.3")
-            .withExposedPorts(6379);
+    @Bean
+    @ServiceConnection(name = "redis")
+    public GenericContainer<?> redisContainer() {
+        return new GenericContainer<>("redis:7.2.3")
+                .withExposedPorts(6379);
+    }
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory("localhost", 6379);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(new StringRedisSerializer());
+        template.afterPropertiesSet();
+        return template;
+    }
 }
--- src/test/java/com/example/study/ratelimit/redis/RedisRateLimitTestControllerTest.java
+++ src/test/java/com/example/study/ratelimit/redis/RedisRateLimitTestControllerTest.java
@@ -1,11 +1,19 @@
 package com.example.study.ratelimit.redis;
 
+import com.example.container.TestContainerConfig;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.util.Objects;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -13,11 +21,31 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Testcontainers
+@Import(TestContainerConfig.class)
+@ActiveProfiles("test")
 @DisplayName("> redis AOP 기반 rate-limit 통합테스트")
 class RedisRateLimitTestControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
+
+    @AfterEach
+    void tearDown() {
+        try {
+            Objects.requireNonNull(redisTemplate.getConnectionFactory())
+                    .getConnection()
+                    .serverCommands()
+                    .flushAll();
+        } catch (Exception e) {
+            // 로그 추가
+            System.err.println("Redis 정리 중 오류 발생: " + e.getMessage());
+        }
+    }
 
     @Test
     @DisplayName("AOP 기반 Redis rate-limit - 5회까지만 허용, 초과 시 429")
